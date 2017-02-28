8f10b1a0afe1f6995c2fc1774e7bf8d189256472
Chandan Kumar2 <Chandan.Kumar2@rsystems.com>
2017-02-28 18:17:41 +0530

    Two way authentication - Email and OTP SMS

---
+++ TwoFactorAuthentication/App_Start/IdentityConfig.cs
@@ -0,0 +1,230 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web;
+using Twilio;
+using System.Diagnostics;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace IdentitySample.Models
+{
+    // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options,
+            IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+            // Configure validation logic for usernames
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false,
+                RequireUniqueEmail = true
+            };
+            // Configure validation logic for passwords
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = true,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireUppercase = true,
+            };
+            // Configure user lockout defaults
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+            // Register two factor authentication providers. This application uses Phone and Emails as a step of receiving a code for verifying the user
+            // You can write your own provider and plug in here.
+            manager.RegisterTwoFactorProvider("PhoneCode", new PhoneNumberTokenProvider<ApplicationUser>
+            {
+                MessageFormat = "Your security code is: {0}"
+            });
+            manager.RegisterTwoFactorProvider("EmailCode", new EmailTokenProvider<ApplicationUser>
+            {
+                Subject = "SecurityCode",
+                BodyFormat = "Your security code is {0}"
+            });
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider =
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+            return manager;
+        }
+    }
+
+    // Configure the RoleManager used in the application. RoleManager is defined in the ASP.NET Identity core assembly
+    public class ApplicationRoleManager : RoleManager<IdentityRole>
+    {
+        public ApplicationRoleManager(IRoleStore<IdentityRole,string> roleStore)
+            : base(roleStore)
+        {
+        }
+
+        public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options, IOwinContext context)
+        {
+            return new ApplicationRoleManager(new RoleStore<IdentityRole>(context.Get<ApplicationDbContext>()));
+        }
+    }
+
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your email service here to send an email.
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Plug in your sms service here to send a text message.
+            TwilioSmsService smsService = new TwilioSmsService();
+            smsService.SendAsync(message);
+            return Task.FromResult(0);
+        }
+    }
+
+    // This is useful if you do not want to tear down the database each time you run the application.
+    // public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    // This example shows you how to create a new database if the Model changes
+    public class ApplicationDbInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext> 
+    {
+        protected override void Seed(ApplicationDbContext context) {
+            InitializeIdentityForEF(context);
+            base.Seed(context);
+        }
+
+        //Create User=Admin@Admin.com with password=Admin@123456 in the Admin role        
+        public static void InitializeIdentityForEF(ApplicationDbContext db) {
+            var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
+            const string name = "admin@example.com";
+            const string password = "Admin@123456";
+            const string roleName = "Admin";
+
+            //Create Role Admin if it does not exist
+            var role = roleManager.FindByName(roleName);
+            if (role == null) {
+                role = new IdentityRole(roleName);
+                var roleresult = roleManager.Create(role);
+            }
+
+            var user = userManager.FindByName(name);
+            if (user == null) {
+                user = new ApplicationUser { UserName = name, Email = name };
+                var result = userManager.Create(user, password);
+                result = userManager.SetLockoutEnabled(user.Id, false);
+            }
+
+            // Add user admin to Role Admin if not already added
+            var rolesForUser = userManager.GetRoles(user.Id);
+            if (!rolesForUser.Contains(role.Name)) {
+                var result = userManager.AddToRole(user.Id, role.Name);
+            }
+        }
+    }
+
+    public class ApplicationSignInManager : SignInManager<ApplicationUser, string>
+    {
+        public ApplicationSignInManager(ApplicationUserManager userManager, IAuthenticationManager authenticationManager) : 
+            base(userManager, authenticationManager) { }
+
+        public override Task<ClaimsIdentity> CreateUserIdentityAsync(ApplicationUser user)
+        {
+            return user.GenerateUserIdentityAsync((ApplicationUserManager)UserManager);
+        }
+
+        public static ApplicationSignInManager Create(IdentityFactoryOptions<ApplicationSignInManager> options, IOwinContext context)
+        {
+            return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
+        }
+    }
+
+    public static class Keys
+    {
+        public static string SMSAccountIdentification = "AC0e445425d2a353e43533f329008fdcff";
+        public static string SMSAccountPassword = "894858a6169252b3c5f662f0810871ed";
+        public static string SMSAccountFrom = "+14243658412";
+        public static string SMSAccountTo = "+919999908412";
+    }
+
+    public class TwilioSmsService : IIdentityMessageService
+    {
+        //public Task SendAsync(IdentityMessage message)
+        //{
+            // Twilio Begin
+             //var Twilio = new TwilioRestClient(Keys.SMSAccountIdentification,Keys.SMSAccountPassword);
+             //var result = Twilio.SendMessage(Keys.SMSAccountFrom,message.Destination, message.Body);
+
+             //Status is one of Queued, Sending, Sent, Failed or null if the number is not valid
+             //Trace.TraceInformation(result.Status);
+             //Twilio doesn't currently have an async API, so return success.
+             //return Task.FromResult(0);
+            // Twilio End
+
+            // ASPSMS Begin 
+            // var soapSms = new WebApplication1.ASPSMSX2.ASPSMSX2SoapClient("ASPSMSX2Soap");
+            // soapSms.SendSimpleTextSMS(
+            //   Keys.SMSAccountIdentification,
+            //   Keys.SMSAccountPassword,
+            //   message.Destination,
+            //   Keys.SMSAccountFrom,
+            //   message.Body);
+            // soapSms.Close();
+            // return Task.FromResult(0);
+            // ASPSMS End
+        //}
+
+
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Twilio Begin
+            //var Twilio = new TwilioRestClient(Keys.SMSAccountIdentification,Keys.SMSAccountPassword);
+            //var result = Twilio.SendMessage(Keys.SMSAccountFrom,message.Destination, message.Body);
+
+            //Status is one of Queued, Sending, Sent, Failed or null if the number is not valid
+            //Trace.TraceInformation(result.Status);
+            //Twilio doesn't currently have an async API, so return success.
+            //return Task.FromResult(0);
+            // Twilio End
+
+            var accountSid = Keys.SMSAccountIdentification;
+            var authToken = Keys.SMSAccountPassword;
+            
+            TwilioClient.Init(accountSid, authToken);
+            PhoneNumber from = new PhoneNumber(Keys.SMSAccountFrom);
+            PhoneNumber to = new PhoneNumber(message.Destination);
+            
+            CreateMessageOptions options = new CreateMessageOptions(to);
+            options.From = from;
+            options.Body = message.Body;
+            var result = MessageResource.Create(options);
+            return Task.FromResult(0);
+        }
+    }
+
+
+}
